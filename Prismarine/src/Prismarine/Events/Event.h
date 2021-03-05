90d72ee7879c64e68e03c5c5f0dd41b88cff016c
Emapex37 <emapex37@gmail.com>
2021-03-05 14:53:49 -0500

    Started event system
    Non-functioning commit. About to make tweaks to premake and am uploading because I am paranoid.

---
+++ Prismarine/src/Prismarine/Events/Event.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Prismarine/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Prismarine {
+
+	//Events are currently blocking
+	//Adding buffer events in the future would be more ideal
+
+
+	//Describes event type
+	enum class EventType {
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	//These can be used to filter events. Uses bit shifting instead of
+	// ints so events can belong to multiple categories.
+	enum EventCategory {
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EventCategoryInput			= BIT(1),
+		EventCategoryKeyboard		= BIT(2),
+		EventCategoryMouse			= BIT(3),
+		EventCategoryMouseButton	= BIT(4)
+	};
+
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType(); }\
+								virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class PRISMARINE_API Event {
+
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category) {
+			return GetCategoryFlags() & category;
+		}
+
+	protected:
+		bool m_Handled = false;
+	};
+
+}
