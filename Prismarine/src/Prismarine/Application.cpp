d1a5722784cf9fc49b79932fa56edb97b02cb41f
Emapex37 <emapex37@gmail.com>
2021-03-05 16:12:32 -0500

    Added new includes
    Application now includes Events.h in header and ApplicationEvent.h, Log.h in .cpp. Log.h also now includes ostr (output stream) from spdlog for custom type logging.

--- Prismarine/src/Prismarine/Application.cpp
+++ Prismarine/src/Prismarine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Prismarine/Events/ApplicationEvent.h"
+#include "Prismarine/Log.h"
+
 namespace Prismarine {
 
 	Application::Application()
@@ -14,6 +17,9 @@ namespace Prismarine {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		PM_TRACE(e);
+
 		while (true);
 	}
 
