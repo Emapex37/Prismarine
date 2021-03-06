e5abf730055360e9a78ee45197e0c55e842206a1
Emapex37 <emapex37@gmail.com>
2021-03-05 22:17:12 -0500

    Window events implemented
    Mouse position, scrolling, and buttons, key activity, window resize, window close. Currently only logs activity, but window close actually terminates program.

--- Prismarine/src/Prismarine/Application.cpp
+++ Prismarine/src/Prismarine/Application.cpp
@@ -1,20 +1,29 @@
 #include "pmpch.h"
 #include "Application.h"
 
-#include "Prismarine/Events/ApplicationEvent.h"
-
 #include "GLFW/glfw3.h"
 
 namespace Prismarine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		PM_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,6 +35,11 @@ namespace Prismarine {
 
 		}
 
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 
 }
